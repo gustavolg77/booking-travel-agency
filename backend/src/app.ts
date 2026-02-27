ee0b45ff82d14e6c3e85d5b4792f659927ccabd5
Luis Gustavo <luisgustavogarcia576@gmail.com>
2026-02-27 11:12:10 -0400

    app ts fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import healthRoutes from "./modules/health/health.routes";
 import authRoutes from "./modules/auth/auth.routes";
+import { authenticate } from "./middlewares/auth.middlewares";
 
 dotenv.config();
 
@@ -11,7 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes); // público
 
-app.use("/api/auth", authRoutes);
+app.use(authenticate); // 🔒 todo lo que sigue es privado
+
+app.use("/api/health", healthRoutes);
 
 export default app;
